139072256e8cb4ba5efc86c7d9551b8a44e69007
Giully314 <giully@protonmail.com>
2026-03-12 21:39:11 +0100

    feat: read cmd args and forward it to editor.

--- app/main.cpp
+++ app/main.cpp
@@ -1,23 +1,27 @@
 import std;
 
-#include <cerrno>
-#include <termios.h> // terminal api
-#include <unistd.h> // read, write
-#include <sys/ioctl.h> // get terminal dimensions
-#include <signal.h> // handle terminal events
-#include <fcntl.h> // flags for non blocking I/O
-
 import mann.config;
 import mann.editor;
 import mann.exceptions;
 
 
-auto main() -> int {
-    mann::Editor editor;
-
+auto main(int argc, const char** argv) -> int {
+    std::unique_ptr<mann::Editor> editor;
+    
+    try {
+        if (argc == 2) {
+            editor = std::make_unique<mann::Editor>(argv[1]);
+        } else {
+            editor = std::make_unique<mann::Editor>();
+        }
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::println("{}", e.what());
+        return 0;
+    }
+    
     try {
         while (true) {
-            editor.run();
+            editor->run();
         }
     } catch (const mann::QuitProgramError& e) {
         return 0;
